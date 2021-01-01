54d060ac5665ed7d94bc227331bc2ef8171c6e8e
nparekh <Neha.Parekh@soti.net>
2020-12-31 22:35:17 -0500

    WebService - GeoLocationAPI

---
+++ neha_practise/webservices_api.py
@@ -0,0 +1,7 @@
+""" WEB SERVICES
+
+API : Application Program Interface
+The API itself is largely abstact in that it specifies an interface and 
+controls the behaviour of the objects specified in that interface. The software that provides the 
+functionality described by an API is said to be an 'implementation' of the API.
+An API is typically defined in terms of the programming language used to build an application """
